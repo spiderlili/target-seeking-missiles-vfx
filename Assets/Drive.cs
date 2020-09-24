aac22000ab965d6645e98ce7a0708ec04019b64b
Jing <jtan1993@gmail.com>
2020-09-24 22:39:46 +0100

    target setup

--- Assets/Drive.cs
+++ Assets/Drive.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drive : MonoBehaviour {
+public class Drive : MonoBehaviour
+{
 
-    public float speed = 10.0f;
+    public float speed = 10.0f; //forward speed
     public float rotationSpeed = 100.0f;
     public float turretSpeed = 0.2f;
     public GameObject turret;
     public GameObject shell;
     public GameObject shellSpawner;
+    public GameObject target;
 
     void Update()
     {
@@ -20,21 +22,29 @@ public class Drive : MonoBehaviour {
         transform.Translate(0, 0, translation);
         transform.Rotate(0, rotation, 0);
 
-        if(Input.GetKey(KeyCode.R))
+        if (Input.GetKey(KeyCode.R)) //pitch the turret back & forward
         {
             turret.transform.Rotate(-turretSpeed, 0, 0);
         }
-        else if(Input.GetKey(KeyCode.F))
+        else if (Input.GetKey(KeyCode.F))
         {
             turret.transform.Rotate(turretSpeed, 0, 0);
         }
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        //Determine the location of where the bullets should go through: based on raycasting from mouse
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit; //project the ray into the environment, receive back any info about something that was hit
+        if (Physics.Raycast(ray.origin, ray.direction, out hit)) //if something is hit
         {
-            GameObject bullet = Instantiate(shell, shellSpawner.transform.position, 
-                                            shellSpawner.transform.rotation);
+            target.transform.position = hit.point;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(shell, shellSpawner.transform.position,
+                shellSpawner.transform.rotation);
             bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * 1000);
         }
-            
+
     }
 }
