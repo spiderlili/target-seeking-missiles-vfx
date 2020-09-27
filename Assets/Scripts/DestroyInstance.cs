1917822ebba71373f83b2d70ad01233d4b2bf197
Jing <jtan1993@gmail.com>
2020-09-27 21:35:06 +0100

    vfx scripts

---
+++ Assets/Scripts/DestroyInstance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyInstance : MonoBehaviour
+{
+    public float deadTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyObject", deadTime);
+    }
+
+    void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/RepeatFire.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatFire : MonoBehaviour
+{
+    public GameObject fireObject;
+    public float timeInterval;
+    private float timeIncrement;
+    private bool isFiring = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isFiring = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isFiring = false;
+        }
+
+        if (isFiring)
+        {
+            timeIncrement += Time.deltaTime;
+            if (timeIncrement >= timeInterval)
+            {
+                Instantiate(fireObject, transform.position, transform.rotation);
+                timeInterval = 0f;
+            }
+        }
+    }
+}
